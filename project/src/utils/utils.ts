511015f52a830c1e1c6fdb171b46b41653f5a3da
Kuchukov Dmitry <kudim@inbox.ru>
2022-04-09 10:04:48 +0300

    add utils functions

---
+++ project/src/utils/utils.ts
@@ -0,0 +1,55 @@
+export const transferRatingToPercent = (rating: number) => Math.round(rating) * 100 / 5;
+
+
+export const setDateToString = (datestring: number) => {
+  const date = new Date(datestring);
+
+  return `${date.toLocaleString('en', {month: 'long'})} ${date.getFullYear()}`;
+};
+
+export const getRandomNumber = (min: number, max: number) => Math.round(min + Math.random() * (max - min));
+
+
+export const toCapitalize = (string: string) => string[0].toUpperCase() + string.slice(1);
+
+
+export const extend = (a: object, b: object) => Object.assign({}, a, b);
+
+// export const getOffersUtils = (offers:[], city:string) => {
+//   return offers.filter((items) => items.city.name === toCapitalize(city));
+// };
+
+export const toCamelCase = (s: string) => s.replace(/([-_][a-z])/ig, (symbol) => (
+  symbol.toUpperCase()
+    .replace('-', '')
+    .replace('_', '')
+));
+
+export const shuffle = (array: []) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array;
+};
+
+
+export const getOfferFavoriteStatus = (isFavorite: boolean) => {
+  if (isFavorite) {
+    return 0;
+  } else {
+    return 1;
+  }
+};
+
+// export const newList = (oldList:[], newObject:{id: number}) => {
+//   return oldList.map((object) => {
+//     if (object.id === newObject.id) {
+//       return newObject;
+//     }
+//     return object;
+//   });
+// };
+//
+// export const deleteObject = (array:[], object:{id: number}) => array.filter((obj) => obj.id !== object.id);
